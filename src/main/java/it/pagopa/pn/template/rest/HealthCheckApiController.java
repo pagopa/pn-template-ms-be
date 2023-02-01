d01a75f90946b2c000e94b6766f09186636d8538
Federico Fatica <117305538+federicofatica-pagopa@users.noreply.github.com>
2023-02-01 08:21:43 +0100

    Update HealthCheckApiController.java

--- src/main/java/it/pagopa/pn/template/rest/HealthCheckApiController.java
+++ src/main/java/it/pagopa/pn/template/rest/HealthCheckApiController.java
@@ -1,10 +1,14 @@
 package it.pagopa.pn.template.rest;
 
 import it.pagopa.pn.template.rest.v1.api.HealthCheckApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Slf4j
+@RestController
 public class HealthCheckApiController implements HealthCheckApi {
 
     @Override
